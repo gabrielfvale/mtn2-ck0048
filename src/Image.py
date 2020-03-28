8d26f4d7c0e15fce9f104463b3358553c73685ff
Gabriel Freire <gabrielfvale@gmail.com>
2020-03-28 18:37:02 -0300

    Apply filters to images

--- src/Image.py
+++ src/Image.py
@@ -1,20 +1,45 @@
 import cv2
 
+import numpy as np
+
 class Image:
 
 
-  def __init__(self, path):
+  def __init__(self, path: str):
     '''
     Loads an image in grayscale
     '''
     self.path = path
-    self.image = cv2.imread(path, 0)
+    # Read image
+    self.image = cv2.imread(path)
+    # Convert to grayscale
+    self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+    # Normalize
+    self.image = cv2.normalize(self.image, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+  def __str__(self) -> str:
+    return self.image.shape
 
 
-  def display(self):
+  def display(self) -> None:
     '''
     Displays the current image in a window.
     '''
+    # Create window with image
     cv2.imshow(self.path, self.image)
+    # Loop until 'Esc' is pressed
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+  def apply_filter(self, filter, iterations):
+    rows, cols = self.image.shape
+    def_img = self.image
+    pad_img = np.pad(def_img, (1, 1), 'constant')
+
+    for k in range(iterations):
+      for i in range(1, rows):
+        for j in range(1, cols):
+          pad_img[i, j] = filter(pad_img, i, j)
+    
+    self.image = pad_img[1:-1, 1:-1].copy()
--- src/main.py
+++ src/main.py
@@ -1,4 +1,23 @@
 from Image import Image
 
+import math
+import numpy as np
+
 corgi = Image('images/corgi.jpg')
+
+
+def box_blur(image, row, col):
+  h = image[row-1, col] + image[row+1, col]
+  v = image[row, col+1] + image[row, col-1]
+  return (h + v)/4
+
+
+def gaussian_blur(image, row, col):
+  h = 2*image[row-1, col] + 2*image[row+1, col]
+  v = 2*image[row, col-1] + 2*image[row, col+1]
+  d = image[row-1, col+1] + image[row+1, col+1] + image[row+1, col-1] + image[row-1, col-1]
+  return (h + v + d)/16
+
+
+corgi.apply_filter(box_blur, 3)
 corgi.display()
